a63588772112aa40a4ef7800087776f628e327c8
jacobguest <124069411+jacobguest@users.noreply.github.com>
2023-09-08 23:58:25 +0100

    Add margin to nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="projects">Projects</Link>
+      <Link className="navbar-item" to="/">Home</Link>
+      <Link className="navbar-item" to="projects">Projects</Link>
     </div>
   );
 };
