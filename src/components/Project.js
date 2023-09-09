4c9e3811be7fb32627bbc18794af920a4807e3ac
jacobguest <124069411+jacobguest@users.noreply.github.com>
2023-09-09 14:09:21 +0100

    Added 3 column layout template

---
+++ src/components/Project.js
@@ -0,0 +1,12 @@
+import './Project.css';
+
+const Project = () => {
+    return (
+        <div className="project-card">
+            <div className="project-thumb">box</div>
+            <div className="project-title">Title</div>
+        </div>
+    );
+};
+
+export default Project;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,27 @@
+import Project from "./Project";
+import "./Projects.css";
+
 const Projects = () => {
-    return(<div>Projects</div>)
+  // media query where if certain size: use conditional for two column, whereas if certain size use 3 column.
+  return (
+    <div className="grid-container">
+      <div className="gridCol">
+        <Project />
+        <Project />
+        <Project />
+      </div>
+      <div className="gridCol">
+        <Project />
+        <Project />
+        <Project />
+      </div>
+      <div className="gridCol">
+        <Project />
+        <Project />
+        <Project />
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
