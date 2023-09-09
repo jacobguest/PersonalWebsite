2b836cde9532ed2458ba707b141cdd0c2654862e
jacobguest <124069411+jacobguest@users.noreply.github.com>
2023-09-09 14:33:17 +0100

    template created for mobile

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,25 +1,45 @@
 import Project from "./Project";
 import "./Projects.css";
 
+import { useMediaQuery } from "react-responsive";
+
 const Projects = () => {
-  // media query where if certain size: use conditional for two column, whereas if certain size use 3 column.
+  const isMobile = useMediaQuery({ maxWidth: 767 }); // Example: screens smaller than 768px
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+
   return (
     <div className="grid-container">
-      <div className="gridCol">
-        <Project />
-        <Project />
-        <Project />
-      </div>
-      <div className="gridCol">
-        <Project />
-        <Project />
-        <Project />
-      </div>
-      <div className="gridCol">
-        <Project />
-        <Project />
-        <Project />
-      </div>
+      {isMobile && <div style={{display: 'flex',height: '100%', width: '100%'}}><div className="gridColDouble">
+            <Project />
+            <Project />
+            <Project />
+            <Project />
+          </div>
+          <div className="gridColDouble">
+            <Project />
+            <Project />
+            <Project />
+            <Project />
+          </div></div>}
+      {isDesktop && (
+        <div style={{display: 'flex',height: '100%', width: '100%'}}>
+          <div className="gridColTriple">
+            <Project />
+            <Project />
+            <Project />
+          </div>
+          <div className="gridColTriple">
+            <Project />
+            <Project />
+            <Project />
+          </div>
+          <div className="gridColTriple">
+            <Project />
+            <Project />
+            <Project />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
