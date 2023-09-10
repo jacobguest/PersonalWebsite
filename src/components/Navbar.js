6466ea096b9a765e36637eaac04ebd3ef9c515a6
jacobguest <124069411+jacobguest@users.noreply.github.com>
2023-09-10 16:36:36 +0100

    Added background & logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,14 @@ import "./Navbar.css";
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+  const logoUrl = "logo2.png";
   return (
     <div className="navbar">
-      <Link className="navbar-item" to="/">Home</Link>
-      <Link className="navbar-item" to="/projects">Projects</Link>
+      <Link className="navbar-item-left" to="/"><img className="logo" src={logoUrl}/></Link>
+      <Link className="navbar-item-right" to="/">Home</Link>
+      <Link className="navbar-item-right" to="/projects">Projects</Link>
+      <Link className="navbar-item-right" to="/projects">About</Link>
     </div>
   );
 };
