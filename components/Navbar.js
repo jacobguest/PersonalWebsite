b0e74e89f6471c2a7c47d13919eb39230bfa72c3
jacobguest <124069411+jacobguest@users.noreply.github.com>
2023-09-15 16:13:30 +0100

    Adjusted navbar for mobile view

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,32 @@
 import styles from "../styles/Navbar.module.css";
 import Link from 'next/link';
+import { useMediaQuery } from "react-responsive";
 
 const Navbar = () => {
 
   const logoUrl = "/logo2.png";
   
+  const isDesktop = useMediaQuery({ minWidth: 1024 });
+
   return (
     <div className={styles.navbar}> 
       <div className={styles['navbar-margin']}>
       <div className={styles['left-links']}>
         <Link href="/" className={styles['navbar-item-left']} ><img alt="JG logo" className={styles.logo} src={logoUrl}/></Link>
       </div>
-      <div className={styles['right-links']}>
-        <Link className={styles['navbar-item-right']} href="/">Home</Link>
-        <Link className={styles['navbar-item-right']} href="/projectspage">Projects</Link>
-        <Link className={styles['navbar-item-right']} href="/">About</Link>
+      <div className={isDesktop ? styles['show-desktop'] : styles['hide-desktop']}>
+        <div className={styles['right-links-desktop']}>
+          <Link className={styles['navbar-item-right']} href="/">Home</Link>
+          <Link className={styles['navbar-item-right']} href="/projectspage">Projects</Link>
+          <Link className={styles['navbar-item-right']} href="/">About</Link>
+        </div>
+      </div>
+      <div className={isDesktop ? styles['hide-desktop'] : styles['show-desktop']}>
+        <div className={styles['right-links-mobile']}>
+          <Link className={styles['navbar-item-right']} href="/">Home</Link>
+          <Link className={styles['navbar-item-right']} href="/projectspage">Projects</Link>
+          <Link className={styles['navbar-item-right']} href="/">About</Link>
+        </div>
       </div>
       </div>
     </div>
